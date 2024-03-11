85093b2c150680b30cd1f84d4a1a078bcab80d03
Maksim <raymax.spb@gmail.com>
2024-03-11 18:14:05 +0300

    Домашнее задание 10 (Строки 1)

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Task 1 ");
         String firstName = "Ivanov";
-        String middleName = "Ivanov";
-        String lastName = "Ivanov";
+        String middleName = "Ivan ";
+        String lastName = "Ivanovich";
         String fullName = firstName+" "+middleName+" "+lastName ;{
             System.out.println("Ф. И. О. сотрудника — "+fullName);}
 
