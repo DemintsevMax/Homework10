31e9e760a730f01055ad663c84e08c2d11378e01
Maksim <raymax.spb@gmail.com>
2024-03-11 18:10:37 +0300

    Домашнее задание 10 (Строки 1)

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Task 1 ");
-        String firstName = "Ivanov";
-        String middleName = "Ivanov";
-        String lastName = "Ivanov";
-        String fullName = firstName+" "+middleName+" "+lastName ;{
-            System.out.println("Ф. И. О. сотрудника — "+fullName);}
 
-        System.out.println("Task 2 ");
-        System.out.println(fullName.toUpperCase());
-
-        System.out.println("Task 3 ");
-        fullName = "Иванов Семён Семёнович";
-        fullName = fullName.replace("ё", "е");
-        System.out.println("Данные Ф. И. О. сотрудника — "+fullName);
 
     }
 
